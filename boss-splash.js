b0610a92935158c8b2d346c76ccdd3f7b5c9774c
jsavko <james@geekwebservices.com>
2024-03-07 09:36:13 -0800

    Added Settings and sounds

--- boss-splash.js
+++ boss-splash.js
@@ -1,14 +1,7 @@
 Hooks.once("init", async function () {
     console.log('Boss Splash Init - Registrering Socket')
     game.socket.on("module.boss-splash", (data) => {
-        let overlay = new game.bossSplash.bossOverlay({actor:data.actor})
-        overlay.render(true)
-        setTimeout(function() {
-            overlay.close({force:true})
-        }, 5000);
-
-
-
+        displayBossOverlay(data.actor);
     })
 
     game.bossSplash = { 
@@ -23,31 +16,114 @@ Hooks.once("init", async function () {
             }
             await game.socket.emit("module.boss-splash", {actor: canvas.tokens.controlled[0].document.actorId});
             //display for yourself
-            let overlay = new game.bossSplash.bossOverlay({actor:canvas.tokens.controlled[0].document.actorId})
-            overlay.render(true)
-
-            setTimeout(function() {
-                overlay.close({force:true})
-            }, 5000);
+            displayBossOverlay(canvas.tokens.controlled[0].document.actorId);
 
         },
         bossOverlay: BossSplashOverlay
 }
 
+    //Register settings
+    game.settings.register("boss-splash", "colorFirst", {
+        name: "SETTINGS.BossSplashColorFirst",
+        hint: "SETTINGS.BossSplashColorFirstHint",
+        scope: "world",
+        type: String,
+        default: '#ffd502',
+        config: true
+    });
+
+    game.settings.register("boss-splash", "colorFirst", {
+        name: "SETTINGS.BossSplashColorFirst",
+        hint: "SETTINGS.BossSplashColorFirstHint",
+        scope: "world",
+        type: String,
+        default: '#ffd502',
+        config: true
+    });
+
+    game.settings.register("boss-splash", "colorSecond", {
+        name: "SETTINGS.BossSplashColorSecond",
+        hint: "SETTINGS.BossSplashColorSecondHint",
+        scope: "world",
+        type: String,
+        default: '#ff8400',
+        config: true
+    });
+
+    game.settings.register("boss-splash", "colorThird", {
+        name: "SETTINGS.BossSplashColorThird",
+        hint: "SETTINGS.BossSplashColorThirdHint",
+        scope: "world",
+        type: String,
+        default: '#ff1f9c',
+        config: true
+    });
+
+    let sounds = 
+    game.settings.register("boss-splash", "bossSound", {
+        name: "SETTINGS.BossSplashSound",
+        hint: "SETTINGS.BossSplashSoundHint",
+        scope: "world",
+        default: null,
+        config: true,
+        type: String,
+        filePicker: true,
+      
+    });
+
 
 });
 
+Hooks.on('renderSettingsConfig', (app, el, data) => {
+    // Insert color picker input
+    el.find('[name="boss-splash.colorFirst"]').parent()
+      .append(`<input type="color" value="${game.settings.get('boss-splash','colorFirst')}" data-edit="boss-splash.colorFirst">`)
+    // Insert preview icon
+    el.find('[data-tab="favorite-items"] h2')
+      .append(` - <i class="favorite-preview fa-solid ${game.settings.get('boss-splash','colorFirst')}" style="color: ${game.settings.get('boss-splash','colorFirst')}"></i>`)//.css({'color':'red'})
+
+          // Insert color picker input
+    el.find('[name="boss-splash.colorSecond"]').parent()
+    .append(`<input type="color" value="${game.settings.get('boss-splash','colorSecond')}" data-edit="boss-splash.colorSecond">`)
+    // Insert preview icon
+    el.find('[data-tab="favorite-items"] h2')
+    .append(` - <i class="favorite-preview fa-solid ${game.settings.get('boss-splash','colorSecond')}" style="color: ${game.settings.get('boss-splash','colorSecond')}"></i>`)//.css({'color':'red'})
+
+        // Insert color picker input
+    el.find('[name="boss-splash.colorThird"]').parent()
+      .append(`<input type="color" value="${game.settings.get('boss-splash','colorThird')}" data-edit="boss-splash.colorThird">`)
+    // Insert preview icon
+    el.find('[data-tab="favorite-items"] h2')
+      .append(` - <i class="favorite-preview fa-solid ${game.settings.get('boss-splash','colorThird')}" style="color: ${game.settings.get('boss-splash','colorThird')}"></i>`)//.css({'color':'red'})
+
+    });
 
 
+function displayBossOverlay(actor) { 
+    let overlay = new game.bossSplash.bossOverlay({actor:actor})
+    overlay.render(true);
+
+    if(!!game.settings.get('boss-splash','bossSound')) {
+        AudioHelper.play({
+            src: game.settings.get('boss-splash','bossSound'),
+            volume: 0.5,
+            autoplay: true,
+            loop: false
+        }, true);
+    }
+
+
+    setTimeout(function() {
+        overlay.close({force:true})
+    }, 5000);
+    
+}
 
     
 export class BossSplashOverlay extends Application {
 
     constructor(...args) {
         super(...args);
-
-        //this.ConflictCaptain = false;
-        //this._initialSidebarWidth = ui.sidebar.element.outerWidth();
     }
 
     /**
@@ -63,7 +139,11 @@ export class BossSplashOverlay extends Application {
             popOut: false,
             classes: ["bossplash"],
             template: "modules/boss-splash/templates/boss-splash.hbs",
-            actor:null
+            actor:null,
+            sound:null, 
+            colorFirst: null,
+            colorSecond: null,
+            colorThird: null
         });
     }
 
@@ -71,10 +151,14 @@ export class BossSplashOverlay extends Application {
     async getData(options={}) {
         const context = super.getData(options);
         context.actor = this.options.actor ?? null;
+        context.colorFirst = this.options.colorFirst ?? game.settings.get('boss-splash','colorFirst');
+        context.colorSecond = this.options.colorSecond ?? game.settings.get('boss-splash','colorSecond');
+        context.colorThird = this.options.colorThird ?? game.settings.get('boss-splash','colorThird');
+        context.sound = this.options.sound ?? null;
         let actor = game.actors.get(context.actor)
         context.actorName = actor.name;
         context.actorImg = actor.img;
-        console.log(context)
+
         return context;
     }    
 
